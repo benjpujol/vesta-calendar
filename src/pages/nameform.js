ab883fc418727d19d56e4a07e011018ba45c9e5b
benjpujol <40798113+benjpujol@users.noreply.github.com>
2022-03-19 13:51:42 +0100

    favicon

--- src/pages/nameform.js
+++ src/pages/nameform.js
@@ -2,9 +2,10 @@ import React, { Component } from "react";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
-import Input from '@mui/material/Input';
-import FormControl from '@mui/material/FormControl';
-import InputLabel from '@mui/material/InputLabel';
+import Input from "@mui/material/Input";
+import FormControl from "@mui/material/FormControl";
+import InputLabel from "@mui/material/InputLabel";
+import { RegisterToAirtable } from "../utils/airtable";
 
 export default class CalendarForm extends Component {
   state = {
@@ -17,6 +18,14 @@ export default class CalendarForm extends Component {
     alert(
       `Login: ${e.target.firstname.value} ${e.target.lastname.value} ${e.target.phone.value}`
     );
+    const base_key = "appabRAbNldKQvYiV";
+    const fields = {
+      Nom: e.target.lastname.value,
+      Prenom: e.target.firstname.value,
+      Phone: e.target.phone.value,
+      Date: "",
+    };
+    RegisterToAirtable(base_key, fields)
   };
 
   onInput = (e) => {
@@ -35,88 +44,85 @@ export default class CalendarForm extends Component {
         component="form"
         sx={{
           "& .MuiTextField-root": { m: 1, width: "30ch" },
-          '& > :not(style)': { m: 1 },
+          "& > :not(style)": { m: 1 },
           display: "flex",
           flexDirection: "column",
           justifyContent: "center",
         }}
-        
         autoComplete="off"
         onSubmit={this.onSubmit}
       >
-          <FormControl variant="standard">
+        <FormControl variant="standard">
           <InputLabel htmlFor="component-simple">Prénom</InputLabel>
-        <Input
-       
-          name="firstname"
-          id="standard-text"
-          label="Prénom"
-          type="text"
-          InputLabelProps={{
-            shrink: true,
-          }}
-          variant="standard"
-          onChange={this.onInput}
-          
-        >
-          {" "}
-          {errors.firstname && (
-            <div
-              style={{ marginTop: "-1.5em" }}
-              className="mui--text-caption mui--text-danger"
-            >
-              {errors.firstname}
-            </div>
-          )}
-        </Input>
+          <Input
+            name="firstname"
+            id="standard-text"
+            label="Prénom"
+            type="text"
+            InputLabelProps={{
+              shrink: true,
+            }}
+            variant="standard"
+            onChange={this.onInput}
+          >
+            {" "}
+            {errors.firstname && (
+              <div
+                style={{ marginTop: "-1.5em" }}
+                className="mui--text-caption mui--text-danger"
+              >
+                {errors.firstname}
+              </div>
+            )}
+          </Input>
         </FormControl>
         <FormControl variant="standard">
           <InputLabel htmlFor="component-simple">Nom</InputLabel>
-        <Input
-          required
-          name="lastname"
-          id="standard-text"
-          label="Nom"
-          type="text"
-          InputLabelProps={{
-            shrink: true,
-          }}
-          variant="standard"
-          invalid={!!errors.lastname}
-        >
-          {errors.lastname && (
-            <div
-              style={{ marginTop: "-1.5em" }}
-              className="mui--text-caption mui--text-danger"
-            >
-              {errors.lastname}
-            </div>
-          )}
-        </Input>
+          <Input
+            required
+            name="lastname"
+            id="standard-text"
+            label="Nom"
+            type="text"
+            InputLabelProps={{
+              shrink: true,
+            }}
+            variant="standard"
+            invalid={!!errors.lastname}
+          >
+            {errors.lastname && (
+              <div
+                style={{ marginTop: "-1.5em" }}
+                className="mui--text-caption mui--text-danger"
+              >
+                {errors.lastname}
+              </div>
+            )}
+          </Input>
         </FormControl>
         <FormControl variant="standard">
           <InputLabel htmlFor="component-simple">Téléphone</InputLabel>
-        <Input
-          required
-          name="phone"
-          id="standard-required"
-          label="Numéro de téléphone"
-          InputLabelProps={{
-            shrink: true,
-          }}
-          variant="standard"
-          invalid={!!errors.phone}
-        >
-          {" "}
-          {errors.phone && (
-            <div
-              style={{ marginTop: "-1.5em" }}
-              className="mui--text-caption mui--text-danger"
-            >
-              {errors.phone}
-            </div>
-          )}
-        </Input>
+          <Input
+            required
+            name="phone"
+            id="standard-required"
+            label="Numéro de téléphone"
+            InputLabelProps={{
+              shrink: true,
+            }}
+            variant="standard"
+            invalid={!!errors.phone}
+          >
+            {" "}
+            {errors.phone && (
+              <div
+                style={{ marginTop: "-1.5em" }}
+                className="mui--text-caption mui--text-danger"
+              >
+                {errors.phone}
+              </div>
+            )}
+          </Input>
         </FormControl>
 
         <Button
