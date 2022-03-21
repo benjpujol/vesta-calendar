1feba9b6d5b70f8e9906bc49ca6ba1d04dfbdfaa
benjpujol <40798113+benjpujol@users.noreply.github.com>
2022-03-21 21:43:03 +0100

    Update datepicker.js

--- src/pages/datepicker.js
+++ src/pages/datepicker.js
@@ -1,6 +1,5 @@
 import * as React from "react";
 import TextField from "@mui/material/TextField";
-
 import frLocale from "date-fns/locale/fr";
 import AdapterDateFns from "@mui/lab/AdapterDateFns";
 import LocalizationProvider from "@mui/lab/LocalizationProvider";
@@ -12,6 +11,7 @@ import Link from "@mui/material/Link";
 import Container from "@mui/material/Container";
 import { useNavigate } from "react-router-dom";
 import { DisponibilitiesList, BookedList } from "../utils/airtable";
+import Grid from "@mui/material/Grid";
 
 import {
   addDays,
@@ -70,19 +70,15 @@ export default function StaticDatePickerDemo() {
               eachHourOfInterval(record).slice(0, -1)
             );
           });
-          
 
           // Remove the slots that were booked before
           let bookedSlotsMilliseconds = bookedSlots.map((item) =>
             item.getTime()
           ); //convert to milliseconds before comparison
 
-          
           let remaining_slots = available_slots.filter(function (el) {
             return !bookedSlotsMilliseconds.includes(el.getTime());
           });
-
-          
 
           //Unique set of Dates where there is at least one remaining slot
           //function to remove duplicates
@@ -94,21 +90,17 @@ export default function StaticDatePickerDemo() {
             .map((item) => startOfDay(item))
             .filter(onlyUnique);
 
-          console.log("remaining", remaining_dates)
+          console.log("remaining", remaining_dates);
 
           //set States
           setAvailableDates(remaining_dates);
           setAvailableSlots(remaining_slots);
         })
-      )
-     ;
+      );
   });
 
   //when value change, find the slots
-  useEffect(() =>setSlots(getTimes(value)), [value])
-
-
-  
+  useEffect(() => setSlots(getTimes(value)), [value]);
 
   function onSelectSlot(item) {
     //navigate to the confirmation page with the selected date as location state
@@ -118,7 +110,6 @@ export default function StaticDatePickerDemo() {
   function getTimes(date) {
     console.log("availableSlots", availableSlots);
     console.log("availableDates", availableDates);
-  
 
     // find all the available slots on the chosen date
     var result = availableSlots.filter(function (item, index) {
@@ -150,70 +141,85 @@ export default function StaticDatePickerDemo() {
 
   return (
     <Container maxWidth="sm" sx={{ padding: "30px 0px 0px" }}>
-      <Box
-        sx={{
-          display: "flex",
-          flexDirection: "row",
-          justifyContent: "flex-start",
-          width: "100%",
-        }}
-      >
-        <h3>Choisissez un créneau pour votre visite</h3>
-      </Box>
-
-      <Box sx={{ display: "flex", flexDirection: "row", alignItems: "center" }}>
-        <Box
-          sx={{
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "flex-start",
-          }}
-        >
-          <LocalizationProvider dateAdapter={AdapterDateFns} locale={frLocale}>
-            <StaticDatePicker
-              displayStaticWrapperAs="desktop"
-              shouldDisableDate={disableDates}
-              openTo="day"
-              minDate={new Date()}
-              maxDate={addDays(new Date(), 15)}
-              views={["day"]}
-              value={value}
-              onChange={(newValue) => {
-                setValue(newValue);
+      <Container maxWidth="md" sx={{ paddingTop: "20px" }}>
+        <img style={{ height: "30px" }} src="/images/vesta.svg" alt="Vesta" />
+      </Container>
+      <Container maxWidth="md" sx={{ paddingTop: "30px" }}>
+        <Grid container spacing={1}>
+          <Grid item xs={12} md={6}>
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "flex-start",
+                width: "100%",
               }}
-              renderInput={(params) => <TextField {...params} />}
-            />
-          </LocalizationProvider>
-        </Box>
-        <Box
-          sx={{
-            display: "flex",
-            flexDirection: "column",
-            height: "100%",
-            position: "relative",
-            width: "50%",
-            mx: "auto",
-            justifyContent: "flex-start",
-          }}
-        >
-          <h4>
-            {value.getDay()
-              ? format(value, "eeee dd MMMM", { locale: frLocale })
-              : ""}
-          </h4>
-          <Stack spacing={2} direction="column">
-            {slots ? (
-              slots.map((item) => (
-                <Button variant="outlined" onClick={() => onSelectSlot(item)}>
-                  {format(item, "HH:mm", { locale: frLocale })}
-                </Button>
-              ))
-            ) : (
-              <></>
-            )}
-          </Stack>
-        </Box>
-      </Box>
+            >
+              <h3 style={{textAlign:"center"}}>Choisissez un créneau pour votre visite</h3>
+            </Box>
+
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "flex-start",
+              }}
+            >
+              <LocalizationProvider
+                dateAdapter={AdapterDateFns}
+                locale={frLocale}
+              >
+                <StaticDatePicker
+                  displayStaticWrapperAs="desktop"
+                  shouldDisableDate={disableDates}
+                  openTo="day"
+                  minDate={new Date()}
+                  maxDate={addDays(new Date(), 15)}
+                  views={["day"]}
+                  value={value}
+                  onChange={(newValue) => {
+                    setValue(newValue);
+                  }}
+                  renderInput={(params) => <TextField {...params} />}
+                />
+              </LocalizationProvider>
+            </Box>
+          </Grid>
+          <Grid item xs={12} md={4}>
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                height: "100%",
+                position: "relative",
+                width: "50%",
+                mx: "auto",
+                justifyContent: "flex-start",
+              }}
+            >
+              <h4>
+                {value.getDay()
+                  ? format(value, "eeee dd MMMM", { locale: frLocale })
+                  : ""}
+              </h4>
+              <Stack spacing={2} direction="column">
+                {slots ? (
+                  slots.map((item) => (
+                    <Button
+                      variant="outlined"
+                      onClick={() => onSelectSlot(item)}
+                    >
+                      {format(item, "HH:mm", { locale: frLocale })}
+                    </Button>
+                  ))
+                ) : (
+                  <></>
+                )}
+              </Stack>
+            </Box>
+          </Grid>
+        </Grid>
+      </Container>
     </Container>
   );
 }
